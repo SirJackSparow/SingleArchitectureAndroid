df22730f4b8ff3789f5219a97e1c83c11d976c72
SirJackSparow <maulanabima785@gmail.com>
2023-05-17 16:59:29 +0700

    drawing path

--- app/src/main/java/com/example/singlearchitecture/widget/DrawingComposeWidget.kt
+++ app/src/main/java/com/example/singlearchitecture/widget/DrawingComposeWidget.kt
@@ -10,8 +10,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,19 +45,53 @@ fun DrawingScaleCompose() {
 }
 
 @Composable
-fun GraphyctLineCompose() {
+fun GraphictLineCompose() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Blue),
+            .background(Color.LightGray),
         contentAlignment = Alignment.Center
     ) {
-        Canvas(modifier = Modifier
-            .padding(8.dp)
-            .aspectRatio(3 / 2f)
-            .fillMaxSize()) {
+        Canvas(
+            modifier = Modifier
+                .padding(8.dp)
+                .aspectRatio(3 / 2f)
+                .fillMaxSize()
+                .drawWithCache {
+                    onDrawBehind {
+
+                    }
+                }
+        ) {
             val barWidthPx = 1.dp.toPx()
             drawRect(Color.DarkGray, style = Stroke(barWidthPx))
+
+            //vertical
+            val verticalLines = 4
+            val verticalSize = size.width / (verticalLines + 1)
+            repeat(verticalLines) { i ->
+                val startX = verticalSize * (i + 1)
+                drawLine(
+                    Color.DarkGray,
+                    start = Offset(startX, 0f),
+                    end = Offset(startX, size.height),
+                    strokeWidth = barWidthPx
+                )
+            }
+
+            //horizontal
+            val horizontalLines = 3
+            val sectionSize = size.height / (horizontalLines + 1)
+            repeat(horizontalLines) { i ->
+                val startY = sectionSize * (i + 1)
+                drawLine(
+                    Color.DarkGray,
+                    start = Offset(0f, startY),
+                    end = Offset(size.width, startY),
+                    strokeWidth = barWidthPx
+                )
+            }
+
         }
     }
 }
@@ -63,6 +100,6 @@ fun GraphyctLineCompose() {
 @Composable
 fun PreviewDrawingCompose() {
     Surface() {
-        GraphyctLineCompose()
+        GraphictLineCompose()
     }
 }
