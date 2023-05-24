4c072a3c78ed7ea707800b82f730380b30fcf828
SirJackSparow <maulanabima785@gmail.com>
2023-05-24 16:53:21 +0700

    architecture

--- app/src/main/java/com/example/singlearchitecture/domain/GetUsersDataUseCase.kt
+++ app/src/main/java/com/example/singlearchitecture/domain/GetUsersDataUseCase.kt
@@ -4,6 +4,6 @@ import com.example.singlearchitecture.data.repositories.GetUserRepo
 import javax.inject.Inject
 
 class GetUsersDataUseCase @Inject constructor(private val repo: GetUserRepo) {
-    suspend fun invoke(q: String, page: Int, sort: String) =
+    suspend operator fun invoke(q: String, page: Int, sort: String) =
         repo.getDataUsers(q = q, page = page, sort = sort)
 }
--- app/src/main/java/com/example/singlearchitecture/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/singlearchitecture/ui/home/HomeViewModel.kt
@@ -3,19 +3,24 @@ package com.example.singlearchitecture.ui.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.singlearchitecture.data.networks.model.UsersRandomModelItem
+import com.example.singlearchitecture.domain.GetUsersDataUseCase
+import com.example.singlearchitecture.utilities.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel : ViewModel() {
+class HomeViewModel @Inject constructor(private val useCaseUser: GetUsersDataUseCase) :
+    ViewModel() {
 
     private var page = 1
     private var query = "super"
     private var sort = ""
+    private var listUsersData = mutableListOf<UsersRandomModelItem>()
     private val _uiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
 
     private val exceptionHandler = CoroutineExceptionHandler { _, _ ->
@@ -24,14 +29,14 @@ class HomeViewModel : ViewModel() {
     val uiState: StateFlow<HomeUiState> get() = _uiState
 
     init {
-
+        loadList()
     }
 
     fun nextPage() {
         viewModelScope.launch(exceptionHandler) {
             if (_uiState.value is HomeUiState.Success && (_uiState.value as HomeUiState.Success).loadNextPage.not()) {
                 page++
-                //_uiState.value = HomeUiState.Success(data = "")
+                _uiState.value = HomeUiState.Success(data = listUsersData, loadNextPage = true)
                 delay(1_000)
                 loadList()
             }
@@ -42,12 +47,27 @@ class HomeViewModel : ViewModel() {
         q: String = query.ifEmpty { "super" },
         sort: String = this.sort
     ) {
+        viewModelScope.launch(exceptionHandler) {
+            useCaseUser(q = q, page = page, sort = sort).collect { result ->
+                when (result) {
+                    is Resource.Success -> {
+                        listUsersData.addAll(result.data ?: emptyList())
+                        _uiState.value = HomeUiState.Success(
+                            data = listUsersData,
+                            loadNextPage = false
+                        )
+                    }
 
+                    is Resource.Error -> _uiState.value = HomeUiState.Error(result.message ?: "")
+                }
+            }
+        }
     }
 }
 
 sealed class HomeUiState {
     object Loading : HomeUiState()
     data class Error(val message: String) : HomeUiState()
-    data class Success(val data: List<UsersRandomModelItem>, val loadNextPage: Boolean) : HomeUiState()
+    data class Success(val data: List<UsersRandomModelItem>, val loadNextPage: Boolean) :
+        HomeUiState()
 }
